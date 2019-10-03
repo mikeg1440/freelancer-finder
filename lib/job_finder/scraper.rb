7d2100c98701cff8aa1071b46ac3796d50c152e7
Mike Gaudreau <gaudreaum01@gmail.com>
2019-10-03 15:22:34 -0400

    Feat: [lib/job_finder/scraper.rb] - Scrape job listings & create job instances
      Created a method scrape_data to scrape the relevant info from the 
    webpage then instantiate a new job instance using those details

--- lib/job_finder/scraper.rb
+++ lib/job_finder/scraper.rb
@@ -5,17 +5,45 @@ class JobFinder::Scraper
   attr_accessor :doc, :base_url
 
   def initialize(url = "https://www.freelancer.com/jobs/")
-    @doc = open_from_url
     @base_url = url
-    scrape_data
+    @doc = open_from_url
+    scrape_data(@doc)
+    self
   end
 
   def open_from_url
     Nokogiri::HTML(open(@base_url))
   end
 
-  def scrape_data
+  def scrape_data(document)
+
+    listings = document.css(".JobSearchCard-item")
+
+    listings.each do |listing|
+
+      info = {}
+
+      info[:title] = listing.css(".JobSearchCard-primary-heading a").text.strip
+      info[:time_left] = listing.css(".JobSearchCard-primary-heading-days").text
+      info[:short_description] = listing.css(".JobSearchCard-primary-description").text.strip
+
+      info[:tags] = listing.css(".JobSearchCard-primary-tags").text.split("\n").map {|tag| tag.strip }  # remove blank entries at some point
+      price_string = listing.css(".JobSearchCard-primary-price").text.strip
+      info[:price] = price_string.match(/\$\d+/)
+      info[:url] = listing.css(".JobSearchCard-primary-heading-link")[0]['href']
+
+      # next create the job instances from the data we just scraped
+
+      JobFinder::Job.new(info)
+
+    end
+
+  end
+
+  def scrape_details
+
     binding.pry
+
   end
 
 
