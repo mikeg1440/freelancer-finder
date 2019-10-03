4efc19aa79d06893a6be8f64ccdf752394494b35
Mike Gaudreau <gaudreaum01@gmail.com>
2019-10-03 15:20:36 -0400

    Feat: [lib/job_finder/job.rb] - Print job instance details
      Created a method called print_details that prints the details of the 
    selected job, also set up the initialize method to take in a hash of job 
    information that then stores the information as instance variables.

--- lib/job_finder/job.rb
+++ lib/job_finder/job.rb
@@ -2,10 +2,38 @@
 
 class JobFinder::Job
 
-  attr_accessor
+  attr_accessor :title, :time_left, :short_description, :tags, :price, :url
 
-  def initialize
+  @@all = []
 
+  def initialize(info)
+    info.each {|key, value| self.send("#{key}=", value) }
+    save
+    self
+  end
+
+  def print_info
+
+    print "Job Title: "
+    print "#{self.title}\n".green
+    print "\tPrice: "
+    print "#{self.price}\n".green
+    print "\tTime Left: "
+    print "#{self.time_left}\n".green
+    print "\tTags: "
+    print "#{self.tags.join(',')}\n".green
+    print "Description: \n"
+    print "#{self.short_description}\n\n".green
+
+  end
+
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
   end
 
 end
