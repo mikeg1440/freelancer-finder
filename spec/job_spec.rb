678ab0172110dc28d1d9c9b564a64ae34274f466
Mike Gaudreau <gaudreaum01@gmail.com>
2020-03-06 13:16:06 -0500

    Create a spec file for job class instances

---
+++ spec/job_spec.rb
@@ -0,0 +1,7 @@
+require 'nokogiri'
+require 'open-uri'
+require_relative '../lib/freelancer_finder/job'
+require_relative '../lib/freelancer_finder/scraper'
+
+RSpec.describe FreelancerFinder::Job do
+end
