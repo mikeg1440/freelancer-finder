aaa440927f9894629a116fd7b9e24811399a9920
Mike Gaudreau <gaudreaum01@gmail.com>
2020-03-06 12:48:03 -0500

    Add test for scrape_recent_jobs function of scraper class

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -3,7 +3,11 @@ require 'open-uri'
 require 'pry-moves'
 require_relative '../lib/freelancer_finder/scraper'
 require_relative '../lib/freelancer_finder/job'
+
+
 RSpec.describe FreelancerFinder::Scraper do
+
+  # scraper = FreelancerFinder::Scraper.new
   scraper = FreelancerFinder::Scraper.new('spec/fixtures/freelancer.com_jobs')
   jobs_hash = scraper.scrape_recent_jobs
 
@@ -14,5 +18,34 @@ RSpec.describe FreelancerFinder::Scraper do
         expect(new_scraper.base_url == 'https://www.freelancer.com/jobs/')
       end
     end
+  end
+
+  describe '#scrape_recent_jobs' do
+    # scraper = FreelancerFinder::Scraper.new('spec/fixtures/freelancer_page_1.html')
+    # jobs_hash = scraper.scrape_recent_jobs
+    it 'scrapes the newest 50 job listings on the first page' do
+      expect(jobs_hash.count == 50)
+    end
+
+    it 'returns a array of hashes' do
+      expect(jobs_hash.class == Array && jobs_hash.all?{ |elem| elem.class == Hash })
+    end
+
+    it 'each hash has a title key' do
+      expect(!jobs_hash.first[:title].empty?)
+    end
+
+    it 'each hash has a short_description key' do
+      expect(!jobs_hash.first[:short_description].empty?)
+    end
+
+    it 'each hash has a path key' do
+      expect(!jobs_hash.first[:path].empty?)
+    end
+
+    it 'each hash has a tags key' do
+      expect(!jobs_hash.first[:tags].empty?)
+    end
+
   end
 end
